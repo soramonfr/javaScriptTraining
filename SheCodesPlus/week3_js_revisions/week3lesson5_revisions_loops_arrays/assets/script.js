9486cfb87953cc09ddf28c6d785e6aec9673d6a8
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-09 00:07:21 +0100

    challenge 3 simplification & challenge 5 update

--- SheCodesPlus/week3_js_revisions/week3lesson5_revisions_loops_arrays/assets/script.js
+++ SheCodesPlus/week3_js_revisions/week3lesson5_revisions_loops_arrays/assets/script.js
@@ -11,7 +11,7 @@ console.log(weekDays[0], weekDays[6]);
 // Challenge 3
 // Change ‘Sunday’ by ‘Funday’
 // Log the array
-weekDays[6] = weekDays[6].replace("Sunday", "Funday");
+weekDays[6] = "Funday";
 console.log(weekDays);
 
 // Challenge 4
@@ -32,3 +32,10 @@ weekDays.forEach(day => {
 // for (const day of weekDays) {
 //     console.log(`Temperature on ${day} is 18 degrees`);
 // }
+
+// Solution préférable
+weekDays.forEach(showTemperature);
+
+function showTemperature (day) {
+    console.log(`Temperature on ${day} is 18 degrees`);
+}
