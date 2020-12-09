b94b3de267e262b9c5fd52202fe18bcfec0a4a84
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-09 23:43:59 +0100

    objects exercises

---
+++ SheCodesPlus/week3_js_revisions/week3lesson6_revisions_objects/assets/script.js
@@ -0,0 +1,59 @@
+// Challenge 1
+let weather = {};
+console.log(weather);
+
+// Challenge 2
+weather = {
+  temperature: 20,
+  humidity: 18
+};
+console.log(weather);
+
+// Challenge 3
+console.log(weather.humidity);
+console.log(weather.temperature);
+
+// Challenge 4
+weather.windSpeed = 5;{
+console.log(weather.windSpeed);
+
+// Challenge 5
+console.log(weather["temperature"]);
+console.log(weather["humidity"]);
+
+// Challenge 6
+let forecast = [
+  {
+    day: "Monday",
+    temperature: 20
+  },
+  {
+    day: "Tuesday",
+    temperature: 26
+  },
+  {
+    day: "Wednesday",
+    temperature: 30
+  },
+  {
+    day: "Thursday",
+    temperature: 29
+  },
+  {
+    day: "Friday",
+    temperature: 15
+  },
+  {
+    day: "Saturday",
+    temperature: 22
+  },
+  {
+    day: "Sunday",
+    temperature: 24
+  }
+];
+console.log(forecast);
+
+// Test
+let sentence = `${forecast[2].day}, the temperature will be ${forecast[2].temperature} degrees.`;
+console.log(sentence);
