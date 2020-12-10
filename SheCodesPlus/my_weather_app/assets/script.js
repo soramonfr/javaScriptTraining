07113b9a264a12767dd9f284377fb23295fc8c21
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-10 23:14:21 +0100

    temp modifications

--- SheCodesPlus/my_weather_app/assets/script.js
+++ SheCodesPlus/my_weather_app/assets/script.js
@@ -20,24 +20,28 @@ let weather = {
         humidity: 20
     }
 };
-console.log(weather);
 
 let city = prompt("Enter a city, please");
-console.log(city);
 city = city.trim();
 city = city.toLowerCase();
 
-if (city === "paris") {
-    alert(`It is currently ${weather["paris"].temp} °C (${weather["paris"].temp + 32}°F) in ${city} with a humidity of ${weather["paris"].humidity} %.`);
-} else if (city === "tokyo") {
-    alert(`It is currently ${weather["tokyo"].temp} °C (${weather["tokyo"].temp + 32}°F) in ${city} with a humidity of ${weather["tokyo"].humidity} %.`);
-} else if (city === "lisbon") {
-    alert(`It is currently ${weather["lisbon"].temp} °C (${weather["lisbon"].temp + 32}°F) in ${city} with a humidity of ${weather["lisbon"].humidity} %.`);
-} else if (city === "san francisco") {
-    alert(`It is currently ${weather["san francisco"].temp} °C (${weather["san francisco"].temp + 32}°F) in ${city} with a humidity of ${weather["san francisco"].humidity} %.`);
-} else if (city === "moscow") {
-    alert(`It is currently ${weather["moscow"].temp} °C (${weather["moscow"].temp + 32}°F) in ${city} with a humidity of ${weather["moscow"].humidity} %.`);
-} else {
-    alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`)
-}
+// if (city === "paris") {
+//     alert(`It is currently ${weather["paris"].temp} °C (${weather["paris"].temp + 32}°F) in ${city} with a humidity of ${weather["paris"].humidity} %.`);
+// } else if (city === "tokyo") {
+//     alert(`It is currently ${weather["tokyo"].temp} °C (${weather["tokyo"].temp + 32}°F) in ${city} with a humidity of ${weather["tokyo"].humidity} %.`);
+// } else if (city === "lisbon") {
+//     alert(`It is currently ${weather["lisbon"].temp} °C (${weather["lisbon"].temp + 32}°F) in ${city} with a humidity of ${weather["lisbon"].humidity} %.`);
+// } else if (city === "san francisco") {
+//     alert(`It is currently ${weather["san francisco"].temp} °C (${weather["san francisco"].temp + 32}°F) in ${city} with a humidity of ${weather["san francisco"].humidity} %.`);
+// } else if (city === "moscow") {
+//     alert(`It is currently ${weather["moscow"].temp} °C (${weather["moscow"].temp + 32}°F) in ${city} with a humidity of ${weather["moscow"].humidity} %.`);
+// } else {
+//     alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`)
+// }
+
+// Simplification du code
+let temperature = weather[city].temp;
+console.log(temperature);
+let temperatureCelsius = temperature;
+let temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
 
