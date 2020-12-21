eb71f5f94f7a246b22ef69fda6092ebef4dc2ef4
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-21 22:53:41 +0100

    lesson7 init

---
+++ SheCodesPlus/week4_js_revisions/week4_lesson7_selectors/assets/script.js
@@ -0,0 +1,12 @@
+// Challenge 1
+// Log li with id special
+
+// Challenge 2
+// Log all li with class of country
+
+// Challenge 3
+// Add class special to the li with id special
+
+// Challenge 4
+// In your code, replace the content of the element with id special
+// by your favorite country (other than Portugal of course)
