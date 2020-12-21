30fe19b31f314f77473c2b7007f811a475a60d60
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-21 23:03:57 +0100

    playing around with selectors

--- SheCodesPlus/week4_js_revisions/week4_lesson7_selectors/assets/script.js
+++ SheCodesPlus/week4_js_revisions/week4_lesson7_selectors/assets/script.js
@@ -1,12 +1,18 @@
 // Challenge 1
 // Log li with id special
+console.log(document.querySelector("li#special"));
 
 // Challenge 2
 // Log all li with class of country
+console.log("li.country");
 
 // Challenge 3
 // Add class special to the li with id special
+let liSpecial = document.querySelector("li#special");
+liSpecial.classList.add("special");
+console.log(liSpecial.className);
 
 // Challenge 4
 // In your code, replace the content of the element with id special
 // by your favorite country (other than Portugal of course)
+liSpecial.innerHTML = "Japan 🍙";
