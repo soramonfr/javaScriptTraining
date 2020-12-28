0aa44adbaab295eaa91d9d06b36b3e833ea8dadb
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 21:15:23 +0100

    get date init

--- SheCodesPlus/my_weather_app/assets/script.js
+++ SheCodesPlus/my_weather_app/assets/script.js
@@ -1,4 +1,15 @@
 // Feature #1 - In your project, display the current date and time using JavaScript
+// id="current-day" (span)
+// id="current-hour" (span)
+// id="city-input" (h2)
+
+let now = new Date();
+let currentDay = document.querySelector("#current-day");
+currentDay.innerHTML = now.getDay();
+
+// now.getMinutes();
+// now.getHours();
+// now.getDay();
 
 // Feature #2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form. 
 
