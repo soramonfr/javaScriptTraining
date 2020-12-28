900fc2758326e04471575c868410355f0d103e47
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 21:07:33 +0100

    js features init

---
+++ SheCodesPlus/my_weather_app/assets/script.js
@@ -0,0 +1,5 @@
+// Feature #1 - In your project, display the current date and time using JavaScript
+
+// Feature #2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form. 
+
+// Feature #3 - Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
