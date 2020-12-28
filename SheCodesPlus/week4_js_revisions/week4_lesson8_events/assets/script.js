efd971f5041d3faa651ad4bd1e6c19332da9b43e
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 18:56:38 +0100

    alert & text replacement done

--- SheCodesPlus/week4_js_revisions/week4_lesson8_events/assets/script.js
+++ SheCodesPlus/week4_js_revisions/week4_lesson8_events/assets/script.js
@@ -5,8 +5,15 @@ function alertTemperature(evt) {
     alert("It is 18 degrees");
 }
 
-let temperatureAlert = document.querySelector("#temperature-btn");
+let temperatureAlert = document.querySelector("#temperature-alert");
 temperatureAlert.addEventListener("click", alertTemperature);
 
 // Challenge 2
 // When the button is clicked, replace button label by ‘18 degrees’
+
+function sayTemperature(evt) {
+    temperatureBtn.innerHTML = "18 degrees";
+}
+
+let temperatureBtn = document.querySelector("#temperature-btn");
+temperatureBtn.addEventListener("click", sayTemperature);
