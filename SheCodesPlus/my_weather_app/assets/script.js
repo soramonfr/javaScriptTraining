663dad8200adcc3186e220d146091442fc3b1c29
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 21:22:39 +0100

    hour display done

--- SheCodesPlus/my_weather_app/assets/script.js
+++ SheCodesPlus/my_weather_app/assets/script.js
@@ -21,9 +21,7 @@ currentDay.innerHTML = days[now.getDay()];
 // Hour display
 
 let currentHour = document.querySelector("#current-hour");
-currentHour.innerHTML = now.getHours();
-
-// now.getMinutes();
+currentHour.innerHTML = `${now.getHours()}:${now.getMinutes()}`;
 
 // Feature #2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form. 
 
