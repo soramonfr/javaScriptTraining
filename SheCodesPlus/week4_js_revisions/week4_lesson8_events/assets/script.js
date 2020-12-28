0439f50135285773ddb03af8ccb8e41f6b571a18
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 18:50:34 +0100

    challenge 1 completed

--- SheCodesPlus/week4_js_revisions/week4_lesson8_events/assets/script.js
+++ SheCodesPlus/week4_js_revisions/week4_lesson8_events/assets/script.js
@@ -1,5 +1,12 @@
 // Challenge 1
 // Add click eventListener to the button. Alert `It is 18 degrees`
 
+function alertTemperature(evt) {
+    alert("It is 18 degrees");
+}
+
+let temperatureAlert = document.querySelector("#temperature-btn");
+temperatureAlert.addEventListener("click", alertTemperature);
+
 // Challenge 2
 // When the button is clicked, replace button label by ‘18 degrees’
