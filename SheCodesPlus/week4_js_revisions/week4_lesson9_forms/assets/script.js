9b97cc4b20b98524147d58fe42ed6864a494b366
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 20:20:39 +0100

    challenge 2 test

--- SheCodesPlus/week4_js_revisions/week4_lesson9_forms/assets/script.js
+++ SheCodesPlus/week4_js_revisions/week4_lesson9_forms/assets/script.js
@@ -6,3 +6,14 @@ function alertMsg() {
 
 let challenge1Btn = document.querySelector("#special-button");
 challenge1Btn.addEventListener("click", alertMsg);
+
+// Challenge 2
+
+function alertPwd(event) {
+    event.preventDefault();
+    let pwdInput = document.querySelector("#password-input");
+    alert(`Your password is ${pwdInput.value}`);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", alertPwd);
