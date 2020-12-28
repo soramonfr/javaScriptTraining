fc69f030511bc6dcbf5af8ddf23fbe29bdbf0980
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 21:28:24 +0100

    search engine test

--- SheCodesPlus/my_weather_app/assets/script.js
+++ SheCodesPlus/my_weather_app/assets/script.js
@@ -1,6 +1,4 @@
 // Feature #1 - In your project, display the current date and time using JavaScript
-// id="current-day" (span)
-// id="current-hour" (span)
 // id="city-input" (h2)
 
 let now = new Date();
@@ -23,6 +21,15 @@ currentDay.innerHTML = days[now.getDay()];
 let currentHour = document.querySelector("#current-hour");
 currentHour.innerHTML = `${now.getHours()}:${now.getMinutes()}`;
 
-// Feature #2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form. 
+// Feature #2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
+
+function cityDisplay(event) {
+    event.preventDefault();
+    let cityInput = document.querySelector("#city-input");
+    cityInput.innerHTML = `${cityInput.value}`;
+}
+
+let formSubmit = document.querySelector("form");
+formSubmit.addEventListener("submit", cityDisplay);
 
 // Feature #3 - Display a fake temperature (i.e 17) in Celsius and add a link to convert it to Fahrenheit. When clicking on it, it should convert the temperature to Fahrenheit. When clicking on Celsius, it should convert it back to Celsius.
