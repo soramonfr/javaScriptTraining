f5805f3dff8004e9e11b8a8b0a57714f464add55
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 19:39:58 +0100

    challenge 2 better practice

--- SheCodesPlus/week4_js_revisions/week4_lesson8_events/assets/script.js
+++ SheCodesPlus/week4_js_revisions/week4_lesson8_events/assets/script.js
@@ -12,7 +12,10 @@ temperatureAlert.addEventListener("click", alertTemperature);
 // When the button is clicked, replace button label by ‘18 degrees’
 
 function sayTemperature(evt) {
-    temperatureBtn.innerHTML = "18 degrees";
+    // temperatureBtn.innerHTML = "18 degrees";
+    // L'event au-dessus marche mais il vaut mieux dupliquer l'elmt btn afin d'avoir une fonction autoportante. 
+    let button = document.querySelector("#temperature-btn");
+    button.innerHTML = "18 degrees";
 }
 
 let temperatureBtn = document.querySelector("#temperature-btn");
