7977d20c1b3b5680815d52669dfd735a2c14bf95
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-28 20:09:56 +0100

    challenge 1

---
+++ SheCodesPlus/week4_js_revisions/week4_lesson9_forms/assets/script.js
@@ -0,0 +1,8 @@
+// Challenge 1
+
+function alertMsg() {
+    alert("Hooray!");
+}
+
+let challenge1Btn = document.querySelector("#special-button");
+challenge1Btn.addEventListener("click", alertMsg);
