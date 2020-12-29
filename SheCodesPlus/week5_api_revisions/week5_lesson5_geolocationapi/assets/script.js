b6f5823302d18e54b0b0e607974cdc885db88d3a
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 22:24:55 +0100

    code improvement

--- SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
@@ -10,14 +10,18 @@ function showTemperature(response) {
     let temperature = Math.round(response.data.main.temp);
     console.log(temperature);
     let h1 = document.querySelector("h1");
-    h1.innerHTML = `The current temperature of your location is 🌡${temperature} celsius degrees.`;
+    h1.innerHTML = `It is currently 🌡${temperature} celsius degrees in ${response.data.name}.`;
 }
+
 function getTemperature(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
+    let unit = "metric";
     let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
-    axios.get(`${apiUrl}&APPID=${apiKey}`).then(showTemperature);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`;
+    axios.get(apiUrl).then(showTemperature);
 }
+
+// Let's first get the temperature of the current position via getTemperature and then show it via showTemperature
 navigator.geolocation.getCurrentPosition(getTemperature);
 
