ad5e557d8b6b3948b3bbd29f87b5ba2eb81ba7df
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 16:06:48 +0100

    city variable for more flexibility

--- SheCodesPlus/week5_api_revisions/week5_lesson4_weatherapi/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson4_weatherapi/assets/script.js
@@ -5,12 +5,20 @@ console.log(axios);
 let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
 // &APPID=c3713b1bcebb5ce5f896fa8a7eec12ab
 
-// Challenge 3
+// Challenge 3 & 4 & 5
+let city = "Sydney";
 function showTemperature(response) {
-    console.log(response.data.main);
+    let temperature = Math.round(response.data.main.temp);
+    console.log(temperature);
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = `It is 🌡${temperature} celsius degrees in ${city}!`;
 }
 
 // First get url API and call it via axios
 // By default, unit mesure is Kelvin so we have to set up the unit we want using &units=metric (celsius) or imperial (fahrenheit)
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=sydney&units=metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
 axios.get(`${apiUrl}&APPID=${apiKey}`).then(showTemperature);
+
+// Challenge 5
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = `It is 🌡${temperature} celsius degrees in Sydney!`;
