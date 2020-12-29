923a46df4c836e2a91ba1507c3378bde026bcda5
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 21:14:12 +0100

    chall 2 tests

--- SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
@@ -6,3 +6,13 @@ function showPosition(position) {
 navigator.geolocation.getCurrentPosition(showPosition);
 
 // Challenge 2
+function showTemperature(response) {
+    console.log(response);
+    let latitude = showPosition(position.coords.latitude);
+    let longitude = showPosition(position.coords.longitude);
+}
+
+let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
+
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
+axios.get(`${apiUrl}&APPID=${apiKey}`).then(showTemperature);
