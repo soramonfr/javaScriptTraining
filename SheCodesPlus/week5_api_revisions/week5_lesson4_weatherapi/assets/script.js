ae48d0262a95fc2baa59dd25480da342a4aba48c
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 15:41:56 +0100

    init ex3

---
+++ SheCodesPlus/week5_api_revisions/week5_lesson4_weatherapi/assets/script.js
@@ -0,0 +1,14 @@
+// Challenge 1
+console.log(axios);
+
+// Challenge 2
+let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
+// &APPID=c3713b1bcebb5ce5f896fa8a7eec12ab
+
+// Challenge 3
+function showTemperature(response) {
+    console.log(response);
+}
+
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=sydney";
+axios.get(apiUrl).then(showTemperature);
