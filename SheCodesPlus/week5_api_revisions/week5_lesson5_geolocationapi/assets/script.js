b6201879e06e6f5554a5f91c7e5c4ae15f4ac3d3
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 21:44:27 +0100

    get and show temp test

--- SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
@@ -5,14 +5,19 @@ function showPosition(position) {
 }
 navigator.geolocation.getCurrentPosition(showPosition);
 
-// Challenge 2
+// Challenge 2 & 3
 function showTemperature(response) {
-    console.log(response);
-    let latitude = showPosition(position.coords.latitude);
-    let longitude = showPosition(position.coords.longitude);
+    let temperature = Math.round(response.data.main.temp);
+    console.log(temperature);
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = `The current temperature of your location is 🌡${temperature} celsius degrees.`;
 }
+function getTemperature(position) {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+    axios.get(`${apiUrl}&APPID=${apiKey}`).then(showTemperature);
+}
+navigator.geolocation.getCurrentPosition(getTemperature);
 
-let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
-
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
-axios.get(`${apiUrl}&APPID=${apiKey}`).then(showTemperature);
