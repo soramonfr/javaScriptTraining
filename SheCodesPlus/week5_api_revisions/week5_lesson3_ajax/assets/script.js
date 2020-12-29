ffe3e8c7fa3fd26d954db3c399a2441f70b38cb9
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 12:57:47 +0100

    chall 3 tests

--- SheCodesPlus/week5_api_revisions/week5_lesson3_ajax/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson3_ajax/assets/script.js
@@ -1,10 +1,10 @@
 // Challenge 2
 
 function displayComments(response) {
-    console.log(response);
+    console.log(response.data[0].email);
 }
 
 let apiUrl = "https://jsonplaceholder.typicode.com/comments";
 axios.get(apiUrl).then(displayComments);
 
-// 
+// Challenge 3
