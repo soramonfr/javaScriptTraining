ec947ae2f65fdc6d503768179494e77574e3883e
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 20:54:52 +0100

    recup coordinates ok

--- SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson5_geolocationapi/assets/script.js
@@ -1,6 +1,8 @@
-
+// Challenge 1
 function showPosition(position) {
-    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
 }
-
 navigator.geolocation.getCurrentPosition(showPosition);
+
+// Challenge 2
