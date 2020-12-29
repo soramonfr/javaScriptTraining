ad237bbebffec71e1aa9dd70d350406878e16a10
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 15:56:00 +0100

    unit conversion & temp log

--- SheCodesPlus/week5_api_revisions/week5_lesson4_weatherapi/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson4_weatherapi/assets/script.js
@@ -7,9 +7,10 @@ let apiKey = "c3713b1bcebb5ce5f896fa8a7eec12ab";
 
 // Challenge 3
 function showTemperature(response) {
-    console.log(response);
+    console.log(response.data.main);
 }
 
+// First get url API and call it via axios
 // By default, unit mesure is Kelvin so we have to set up the unit we want using &units=metric (celsius) or imperial (fahrenheit)
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=sydney&units=metric";
 axios.get(`${apiUrl}&APPID=${apiKey}`).then(showTemperature);
