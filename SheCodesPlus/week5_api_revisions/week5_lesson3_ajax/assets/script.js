d78cb9db9d115eaa014f0c083caf80e16d34da05
sorayacruchet <soraya.cruchet@gmail.com>
2020-12-29 13:03:30 +0100

    done

--- SheCodesPlus/week5_api_revisions/week5_lesson3_ajax/assets/script.js
+++ SheCodesPlus/week5_api_revisions/week5_lesson3_ajax/assets/script.js
@@ -1,10 +1,14 @@
-// Challenge 2
+// Challenge 1
+console.log(axios);
 
+// Challenge 2 & 3
 function displayComments(response) {
+    // Challenge 2
+    console.log(response); 
+    // Challenge 3
     console.log(response.data[0].email);
 }
 
+// First step is to define where and how to get our data, then call it via a function (displayComments for this case)
 let apiUrl = "https://jsonplaceholder.typicode.com/comments";
 axios.get(apiUrl).then(displayComments);
-
-// Challenge 3
